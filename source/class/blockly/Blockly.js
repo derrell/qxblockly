f69412a836339934b730a61415436c8a76c7c33b
Derrell Lipman <derrell.lipman@unwireduniverse.com>
2012-05-22 12:16:37 -0400

    allow language map to be passed to constructor; use it when blockly is loaded

--- source/class/blockly/Blockly.js
+++ source/class/blockly/Blockly.js
@@ -26,8 +26,16 @@ qx.Class.define("blockly.Blockly",
 
   /**
    * Create the Blockly widget
+   *
+   * @param blocks {Map}
+   *   Map, where each key is an member added to Blockly.Language, and the
+   *   value is itself a map, in the format required by Blockly.Language. In
+   *   addition to those elements of the map required by Blockly, there may
+   *   (should) also be a <i>generators</i> map which contains one member for
+   *   each output language supported. The value of that that member should be
+   *   the function for generating code in that language.
    */
-  construct : function() 
+  construct : function(blocks)
   {
     var             blocklyLoader;
     var             vBox;
@@ -36,6 +44,9 @@ qx.Class.define("blockly.Blockly",
     // horizontally.
     this.base(arguments, new qx.ui.layout.HBox());
     
+    // Save the blocks map. We'll use it after Blockly is loaded.
+    this.__blocks = blocks || {};
+
     // Start loading all of the Blockly files
     blocklyLoader = blockly.BlocklyLoader.getInstance();
     
@@ -91,6 +102,8 @@ qx.Class.define("blockly.Blockly",
   {
     __eventsPending : 0,
 
+    __language : null,
+
     /**
      * Called when all Blockly files have been loaded, and when the user
      * interface has received its "appear" event. Once both of these have
@@ -103,15 +116,6 @@ qx.Class.define("blockly.Blockly",
      */
     _startBlockly : function(e)
     {
-      var             nodes;
-      var             root;
-      var             parent;
-      var             prefixLength;
-      var             category;
-      var             categoryList;
-      var             categoryDecoded;
-      var             nodeTree;
-
       // Was this a "done" event from the Blockly loader?
       if (e)
       {
@@ -129,183 +133,246 @@ qx.Class.define("blockly.Blockly",
       // Have we received all that we expect?
       if (this.__eventsPending == 0)
       {
-        // Yup. Start up Blockly. First, make some path changes.
-        Blockly.makeFilename = function(name)
-        {
-          return qx.util.ResourceManager.getInstance().toUri(
-            "upstream/" + name);
-        };
-        Blockly.Trashcan.prototype.BODY_URL_ =
-          qx.util.ResourceManager.getInstance().toUri(
-            "upstream/media/trashbody.png");
-        Blockly.Trashcan.prototype.LID_URL_ =
-          qx.util.ResourceManager.getInstance().toUri(
-            "upstream/media/trashlid.png");
-
-        // Do not display the SVG toolbox. We have our own.
-        Blockly.features.disableSvgToolbox = true;
-        
-        // Now start up Blockly.
-        Blockly.inject(this.editor.getContentElement().getDomElement());
-        
-        // Resize the blocks editor when its container resizes.
-        this.editor.addListener(
-          "resize",
-          function(e)
-          {
-            // Delay momentarily, so container size has actually changed
-            qx.util.TimerManager.getInstance().start(
-              function(e)
-              {
-                Blockly.fireUiEvent(Blockly.svgDoc, window, 'resize');
-              });
-          },
-          this);
-
-        // Override the toolbox's clearSelection method to remove the
-        // selection from our tree instead of from the SVG (internal) tree.
-        Blockly.Toolbox.clearSelection = qx.lang.Function.bind(
-          function()
-          {
-            Blockly.Toolbox.flyout_.hide();
-            this.tree.getSelection().removeAll();
-          },
-          this);
-
-        // Populate the toolbox tree
-        nodes = [];
-
-        // The root node is not shown
-        nodes[0] = 
-          {
-            name     : "<root>",
-            children : []
-          };
-
-        root = nodes[0];
-        prefixLength = Blockly.Toolbox.PREFIX_.length;
-
-        // Initialize the node tree
-        nodeTree = 
-          {
-            "<root>" : 
-            {
-              node     : nodes[0],
-              children : {}
-            }
-          };
-
-        for (category in Blockly.Toolbox.languageTree)
-        {
-          categoryDecoded = window.decodeURI(category.substring(prefixLength));
-          categoryList = categoryDecoded.split("\0");
-
-          // Start at the root, in search of the specified hierarchy
-          parent = nodeTree["<root>"];
-          
-          categoryList.forEach(
-            function(nodeName, i)
-            {
-              var             node; 
-              
-              // Add this name and its node reference to the name hierarchy.
-              // Does a node for this name hierarchy already exist? If so,
-              // there's nothing for us to do.
-              if (! parent.children[nodeName])
-              {
-                // This node does not already exist. Create the new node for
-                // the data model.
-                node =
-                  {
-                    name     : nodeName
-                  };
-
-                // Is this a leaf node?
-                if (i == categoryList.length - 1)
-                {
-                  // Yup. Add the category.
-                  node.category = category;
-                }
-                else
-                {
-                  // It's a branch node. Add a children array.
-                  node.children = [];
-                }
-
-                // Add this new node.
-                parent.node.children.push(node);
-
-                // Keep track of the hierarchy in an efficient fashion, and
-                // advance to the new child node.
-                parent = parent.children[nodeName] =
-                  {
-                    node     : node,
-                    children : []
-                  };
-              }
-              else
-              {
-                parent = parent.children[nodeName];
-              }
-            },
-            this);
-        }
-
-        // converts the raw nodes to qooxdoo objects
-        nodes = qx.data.marshal.Json.createModel(nodes, true);
-
-        // Create a listener for when the selection changes
-        this.tree.getSelection().addListener(
-          "change",
-          function(e)
-          {
-            var             selection;
-            var             category;
-
-            // Retrieve the selection
-            selection = this.tree.getSelection();
-            
-            // See if anything is selected
-            if (selection.getLength() === 0)
-            {
-              // Nope, nothing is selected, so we have nothing to do
-              return;
-            }
-            
-            // Hide any previous flyout that may still be visible
-            Blockly.Toolbox.flyout_.hide();
-
-            // Is this a leaf node with a category?
-            if (! selection.getItem(0).getCategory)
-            {
-              // Nope. Deselect it. (Doesn't work if not delayed a bit.)
-              qx.util.TimerManager.getInstance().start(
-                function()
-                {
-                  selection.removeAll();
-                },
-                0,
-                this,
-                null,
-                10);
-              
-              // No more to do.
-              return;
-            }
-
-            // Get the category of the selected item
-            category = selection.getItem(0).getCategory();
-            
-            // Display the flyout associated with the selected category.
-            var blockSet = Blockly.Toolbox.languageTree[category] || category;
-            Blockly.Toolbox.flyout_.show(blockSet);
-          },
-          this);
-
-        this.tree.setModel(nodes.getItem(0));
+        // Yup. Blockly is read
+        this._lockedAndLoaded();
       }
     },
     
+    /**
+     * Called when Blockly is fully loaded and we've received the "appear"
+     * event on the user interface.
+     */
+    _lockedAndLoaded : function()
+    {
+      var             nodes;
+      var             root;
+      var             parent;
+      var             prefixLength;
+      var             category;
+      var             categoryList;
+      var             categoryDecoded;
+      var             nodeTree;
+      var             block;
+      var             blockName;
+      var             language;
+
+      // Yup. Start up Blockly. First, make some path changes.
+      Blockly.makeFilename = function(name)
+      {
+        return qx.util.ResourceManager.getInstance().toUri(
+          "upstream/" + name);
+      };
+      Blockly.Trashcan.prototype.BODY_URL_ =
+        qx.util.ResourceManager.getInstance().toUri(
+          "upstream/media/trashbody.png");
+      Blockly.Trashcan.prototype.LID_URL_ =
+        qx.util.ResourceManager.getInstance().toUri(
+          "upstream/media/trashlid.png");
+
+      // Do not display the SVG toolbox. We have our own.
+      Blockly.features.disableSvgToolbox = true;
+
+      // Were any languages loaded along with Blockly?
+      if (! Blockly.Language)
+      {
+        // Nope. Initialize the language map.
+        Blockly.Language = {};
+      }
+      
+      // Add any language components provided to our constructor.
+      for (blockName in this.__blocks)
+      {
+        // Reference the block to be saved
+        block = this.__blocks[blockName];
+
+        // Save this block description
+        Blockly.Language[blockName] = block;
+        
+        // If there's a map of output language generation functions...
+        if (block.generators)
+        {
+          for (language in block.generators)
+          {
+            // Create or retrieve this language's map from within Blockly
+            Blockly[language] = Blockly.Generator.get(language);
+            
+            // Save the language generator for this language, for this block
+            Blockly[language][blockName] = block.generators[language];
+          }
+        }
+      }
+
+      // Now start up Blockly.
+      Blockly.inject(this.editor.getContentElement().getDomElement());
+
+      // Resize the blocks editor when its container resizes.
+      this.editor.addListener(
+        "resize",
+        function(e)
+        {
+          // Delay momentarily, so container size has actually changed
+          qx.util.TimerManager.getInstance().start(
+            function(e)
+            {
+              Blockly.fireUiEvent(Blockly.svgDoc, window, 'resize');
+            });
+        },
+        this);
+
+      // Override the toolbox's clearSelection method to remove the
+      // selection from our tree instead of from the SVG (internal) tree.
+      Blockly.Toolbox.clearSelection = qx.lang.Function.bind(
+        function()
+        {
+          Blockly.Toolbox.flyout_.hide();
+          this.tree.getSelection().removeAll();
+        },
+        this);
+
+      // Populate the toolbox tree
+      nodes = [];
+
+      // The root node is not shown
+      nodes[0] = 
+        {
+          name     : "<root>",
+          children : []
+        };
+
+      root = nodes[0];
+      prefixLength = Blockly.Toolbox.PREFIX_.length;
+
+      // Initialize the node tree
+      nodeTree = 
+        {
+          "<root>" : 
+          {
+            node     : nodes[0],
+            children : {}
+          }
+        };
+
+      for (category in Blockly.Toolbox.languageTree)
+      {
+        categoryDecoded = window.decodeURI(category.substring(prefixLength));
+        categoryList = categoryDecoded.split("\0");
+
+        // Start at the root, in search of the specified hierarchy
+        parent = nodeTree["<root>"];
+
+        categoryList.forEach(
+          function(nodeName, i)
+          {
+            var             node; 
+
+            // Add this name and its node reference to the name hierarchy.
+            // Does a node for this name hierarchy already exist? If so,
+            // there's nothing for us to do.
+            if (! parent.children[nodeName])
+            {
+              // This node does not already exist. Create the new node for
+              // the data model.
+              node =
+                {
+                  name     : nodeName
+                };
+
+              // Is this a leaf node?
+              if (i == categoryList.length - 1)
+              {
+                // Yup. Add the category.
+                node.category = category;
+              }
+              else
+              {
+                // It's a branch node. Add a children array.
+                node.children = [];
+              }
+
+              // Add this new node.
+              parent.node.children.push(node);
+
+              // Keep track of the hierarchy in an efficient fashion, and
+              // advance to the new child node.
+              parent = parent.children[nodeName] =
+                {
+                  node     : node,
+                  children : []
+                };
+            }
+            else
+            {
+              // This node already exists. Just move on to the next one.
+              parent = parent.children[nodeName];
+            }
+          },
+          this);
+      }
+
+      // Convert the raw nodes to qooxdoo objects
+      nodes = qx.data.marshal.Json.createModel(nodes, true);
+
+      // Create a listener for when the selection changes
+      this.tree.getSelection().addListener(
+        "change", this._onSelectionChange, this);
+
+      // Render this model in the tree
+      this.tree.setModel(nodes.getItem(0));
+    },
+
+    /**
+     * Event handler for a change of selection on the toolbox tree
+     */
+    _onSelectionChange : function()
+    {
+      var             selection;
+      var             category;
+
+      // Retrieve the selection
+      selection = this.tree.getSelection();
+
+      // See if anything is selected
+      if (selection.getLength() === 0)
+      {
+        // Nope, nothing is selected, so we have nothing to do
+        return;
+      }
+
+      // Hide any previous flyout that may still be visible
+      Blockly.Toolbox.flyout_.hide();
+
+      // Is this a leaf node with a category?
+      if (! selection.getItem(0).getCategory)
+      {
+        // Nope. Deselect it. (Requires a delay to be functional.)
+        qx.util.TimerManager.getInstance().start(
+          function()
+          {
+            selection.removeAll();
+          },
+          0,
+          this,
+          null,
+          10);
+
+        // No more to do.
+        return;
+      }
+
+      // Get the category of the selected item
+      category = selection.getItem(0).getCategory();
+
+      // Display the flyout associated with the selected category.
+      var blockSet = Blockly.Toolbox.languageTree[category] || category;
+      Blockly.Toolbox.flyout_.show(blockSet);
+    },
+
+    /**
+     * Convert the current blocks program into JavaScript code.
+     * 
+     * @return {String}
+     *   The JavaScript representation of the current blocks program.
+     */
     toJavaScript : function()
     {
       return Blockly.Generator.workspaceToCode('JavaScript');      
